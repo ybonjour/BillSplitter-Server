70148215186ede165ae5ca64e2b98b95f64fb785
Dominik Giger <dominik.giger@ergon.ch>
2015-12-28 15:29:48 +0100

    Updated Spring Boot, switched server to HTTPS

--- server/src/main/java/ch/pantas/billsplitter/server/Application.java
+++ server/src/main/java/ch/pantas/billsplitter/server/Application.java
@@ -4,13 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @ComponentScan
 @EnableAutoConfiguration
 @Configuration
-@PropertySource("classpath:application.properties")
 public class Application extends WebMvcConfigurerAdapter {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
